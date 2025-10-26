67b9643a6dd0df5c82d1c6bfed409f6252b5706f
kevins1257 <shahkevk@gmail.com>
2025-10-26 12:20:50 -0400

    Checking for status

---
+++ Hello.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Author: Kevin Shah
+
+
+int main() {
+    
+   int a , b;
+   printf("enter a : ");
+   scanf ("%d", &a);
+
+   printf("enter b : ");
+   scanf ("%d", &b);
+
+   printf("Sum is : %d \n", a + b);
+
+
+
+    
+
+    return 0;
+}
+
